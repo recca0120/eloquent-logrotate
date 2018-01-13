0ed6321cacd7dd1965116830e8db4d6c35775ae4
recca0120 <recca0120@gmail.com>
2018-01-13 23:29:40 +0800

    setup database connection

--- tests/LogRotateTest.php
+++ tests/LogRotateTest.php
@@ -5,18 +5,36 @@ namespace Recca0120\LaravelTracy\Tests;
 use Mockery as m;
 use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
 use Illuminate\Database\Eloquent\Model;
 use Recca0120\EloquentLogRotate\LogRotate;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 class LogRotateTest extends TestCase
 {
+    private $capsule;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->capsule = new Capsule;
+        $this->capsule->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+        ]);
+        $this->capsule->setEventDispatcher(new Dispatcher(new Container));
+        $this->capsule->setAsGlobal();
+        $this->capsule->bootEloquent();
+    }
+
     protected function tearDown()
     {
         parent::tearDown();
         m::close();
     }
 
-    public function testLogRotateDaily()
+    public function testLogRotate()
     {
         $now = Carbon::now();
         $log = new Log();
